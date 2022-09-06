ca2a2ad90c2de52244a806cf9124a0fb6dfbc49b
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-09-06 11:40:14 -0500

    add react-dom dependency.

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 const mount = ( el ) => {
     ReactDOM.render(
@@ -11,7 +12,7 @@ if (process.env.NODE_ENV === 'development'){
     const devRoot = document.querySelector('#_marketing-dev-root');
     
     if(devRoot){
-        mount(_marketing-dev-root);
+        mount(devRoot);
     }
 }
 
