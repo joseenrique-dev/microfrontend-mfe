ef50e19b462e00ec4cb28327cb50e7bfa06ed3ed
José Enrique López Obeso <jose_lopez-obeso@bio-rad.com>
2022-10-01 17:59:57 -0500

    fail regeneration action, try again

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+//push container code.
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
