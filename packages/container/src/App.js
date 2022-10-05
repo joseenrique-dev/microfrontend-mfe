b4da05d3936496a398c9eefdaa8f7cb1b2f86968
José Enrique <joseenrique.dev@gmail.com>
2022-10-05 08:34:47 -0500

    text changes.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 //TODO:Change this...
 export default () =>{
     return <div>
-        <h1>Hi there from container package!!!!</h1>
+        <h1>Hi there from container package!!!</h1>
         <hr />
         <MarketingApp />
     </div>
