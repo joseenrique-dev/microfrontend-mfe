73e963e92f6ae747c73263edab546e208cc12826
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-11-02 23:07:01 -0500

    using Header component into container app.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
 export default () =>{
-    return <div>
-        <h1>Hi there from container package!!!</h1>
-        <hr />
-        <MarketingApp />
-    </div>
+    return (<div>
+                <BrowserRouter>
+                    <Header />
+                    <MarketingApp />
+                </BrowserRouter>
+            </div>);    
 }
